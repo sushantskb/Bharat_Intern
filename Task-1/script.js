7d2db8f219591fff6ac01a1ac9088f5e640b4e17
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-05-25 01:58:27 +0530

    Added the about section and fixed some bugs

--- Task-1/script.js
+++ Task-1/script.js
@@ -5,3 +5,13 @@ function stickyNavbar() {
 }
 stickyNavbar();
 window.addEventListener("scroll", stickyNavbar);
+// --end--
+
+// 11th code continues here, adding the scroll reveal animation
+let sr = ScrollReveal({
+    duration: 2500,
+    distance: "60px",
+});
+sr.reveal(".showcase-info", {delay: 600});
+sr.reveal(".showcase-image", {origin: "left", delay: 700});
+
